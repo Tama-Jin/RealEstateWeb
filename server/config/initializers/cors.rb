e8a19ecbc40ac0d9bc7c8ef10013f7ff64790400
Tama-Jin <behi930321@gmail.com>
2025-02-02 22:17:57 +0900

    Fix: Force push latest local changes

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'  # 리액트 앱의 포트 (3001)
+    origins 'http://localhost:3001'  
     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
   end
 end
